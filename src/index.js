ebbe46a433ca4bae22d4e802d55380ff0c5e78bb
Lilit Khamalyan <lilitkhamalyan@gmail.com>
2020-02-02 12:36:47 -0500

    Created my first functional component.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// 1. Set up the basic React code from scratch
+// 2. Create a functional component called MyInfo that returns the following UI:
+// a. An h1 with your name
+// b. A paragraph with a little blurb about yourself
+// c. An ordered or unordered list of the top 3 vacation spots you'd
+//    like to visit
+//3. Render an instance of that functional component to the browser
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react"
+import ReactDom from "react-dom"
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function MyInfo() {
+  return (
+    <div>
+      <h1>Lilit Khamalyan</h1>
+      <p>Learning React JS.</p>
+      <ul>
+        <li>France</li>
+        <li>Canada</li>
+        <li>London</li>
+      </ul>
+    </div>
+  )
+}
+ReactDom.render(
+  <MyInfo />,
+  document.getElementById("root")
+)
